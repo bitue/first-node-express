90035a8b3c48076ec7b1ca44c463efdc89ddc1e4
bitueboss <69850346+bitueboss@users.noreply.github.com>
2021-10-23 13:41:46 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(cors())
+
+app.use(express.json())
+
+const port = 5000;
+
+
+const user = [
+    {id:0,name:'ashik', email : "ashik@gmail.com"},
+    {id:1,name:'ashik', email : "ashik@gmail.com"},
+    {id:2,name:'ashik', email : "ashik@gmail.com"},
+    {id:3,name:'ashik', email : "ashik@gmail.com"}
+]
+
+app.get('/', (req, res) => {
+    res.send('Hello ')
+  })
+
+ 
+
+app.post('/ashik', (req , res)=> {
+    console.log('hitting the post',req.body)
+    const newUser = req.body
+    newUser.id = user.length
+    user.push(newUser)
+    res.send('inside post ')
+    console.log(user)
+})
+
+app.get('/ashik', (req, res)=> {
+    
+    res.send(user)
+
+}) 
+
+  
+app.listen(port , ()=> {
+    console.log('connecting user from', port  )
+})
